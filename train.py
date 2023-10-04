739714ecfe4cca30a6293461056f89a8e35911d7
Maximrez <maximrez@mail.ru>
2023-10-04 16:35:45 +0300

    train

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ import os
 import pandas as pd
 import numpy as np
 import random
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning)
 
 seed = 123
 torch.manual_seed(seed)
@@ -24,16 +27,18 @@ TRAIN_VISIONENCODERDECODERMODEL_PATH = "./train/visionencoderdecodermodel"
 TRAIN_TOKENIZER_PATH = "./train/tokenizer"
 TRAIN_IMAGEPROCESSOR_PATH = "./train/imageprocessor"
 
+ACCESS_TOKEN = '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'
+
 
 def init_model(device):
     # encoder_model = "google/vit-base-patch16-224"
     encoder_model = "google/vit-base-patch16-224-in21k"
-    # encoder_model = "vit-rugpt2-image-captioning"
+    # encoder_model = "tuman/vit-rugpt2-image-captioning"
     # encoder_model = "microsoft/swin-base-patch4-window7-224-in22k"
 
     # decoder_model = "bert-base-multilingual-cased"
     # decoder_model = "Helsinki-NLP/opus-mt-ru-en"
-    # decoder_model = "vit-rugpt2-image-captioning"
+    # decoder_model = "tuman/vit-rugpt2-image-captioning"
     decoder_model = "sberbank-ai/rugpt3large_based_on_gpt2"
 
     tokenizer = AutoTokenizer.from_pretrained(decoder_model)
@@ -41,16 +46,16 @@ def init_model(device):
 
     image_processor = ViTImageProcessor.from_pretrained(encoder_model)
 
-    model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(encoder_model, decoder_model).to(device)
-    # model = VisionEncoderDecoderModel.from_pretrained("vit-rugpt2-image-captioning").to(device)
+    # model = VisionEncoderDecoderModel.from_encoder_decoder_pretrained(encoder_model, decoder_model).to(device)
+    model = VisionEncoderDecoderModel.from_pretrained(decoder_model).to(device)
 
-    tokenizer.pad_token = tokenizer.eos_token
+    # tokenizer.pad_token = tokenizer.eos_token
     # model.config.decoder_start_token_id = tokenizer.cls_token_id
-    model.config.decoder_start_token_id = tokenizer.bos_token_id
-    model.config.pad_token_id = tokenizer.pad_token_id
+    # model.config.decoder_start_token_id = tokenizer.bos_token_id
+    # model.config.pad_token_id = tokenizer.pad_token_id
     # model.config.eos_token_id = tokenizer.sep_token_id
-    model.config.max_new_tokens = 64  # максимальное количество слов в датасете - 64
-    model.decoder.resize_token_embeddings(len(tokenizer))
+    # model.config.max_new_tokens = 64  # максимальное количество слов в датасете - 64
+    # model.decoder.resize_token_embeddings(len(tokenizer))
 
     return model, tokenizer, image_processor
 
@@ -138,15 +143,24 @@ def init_dataloader(batch_size=5):
         return tuple(zip(*batch))
 
     captions_df = pd.read_csv(os.path.join('data', 'captions_ru.csv'), index_col='index')
-    images = list(captions_df['image'])[:1000]
-    captions = list(captions_df['caption_ru'])[:1000]
+    images = list(captions_df['image'])
+    captions = list(captions_df['caption_ru'])
 
-    my_dataset = MyDataset(data_dir='/home/msreznik/PythonProjects/DICT/data/images',
-                           names=images,
-                           captions=captions)
-    # my_dataset = MyDataset(data_dir='data/images',
+    def transform_caption(caption: str) -> str:
+        for s in '.,!?:;':
+            caption = caption.replace(s, '')
+
+        caption = ' '.join(caption.split()).lower()
+        return caption
+
+    # captions = list(map(transform_caption, captions))
+
+    # my_dataset = MyDataset(data_dir='/home/msreznik/PythonProjects/DICT/data/images',
     #                        names=images,
     #                        captions=captions)
+    my_dataset = MyDataset(data_dir='data/images',
+                           names=images,
+                           captions=captions)
 
     return torch.utils.data.DataLoader(my_dataset,
                                        batch_size=batch_size,
@@ -162,7 +176,7 @@ def save_offline_models():
     model.save_pretrained(VISIONENCODERDECODERMODEL_PATH)
 
 
-def test_picture(device, model, tokenizer, image_processor, file_path):
+def test_picture(device, model, tokenizer, image_processor, file_path, verbose=False):
     model.eval()
 
     img = Image.open(file_path)
@@ -171,24 +185,31 @@ def test_picture(device, model, tokenizer, image_processor, file_path):
 
     generated_ids = model.generate(pixel_values)
 
-    generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    if verbose:
+        generated_text_list = tokenizer.batch_decode(generated_ids, skip_special_tokens=False)
+        print(generated_text_list)
+
+    generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
     return generated_text
 
 
 if __name__ == '__main__':
     # save_offline_models()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("device:", device)
+    # device_name = "cuda" if torch.cuda.is_available() else "cpu"
+    device_name = "cpu"
+    device = torch.device(device_name)
+    print("device:", device_name)
 
-    model, tokenizer, image_processor = init_model_offline(device, train_path=False)
+    model, tokenizer, image_processor = init_model_offline(device)
     # model, tokenizer, image_processor = init_model(device)
-    loss_history, model, tokenizer, image_processor = train(device, model, tokenizer, image_processor, init_dataloader(), num_epochs=3)
-    print(loss_history)
-    with open('loss_history.txt', 'w', encoding='utf-8') as f:
-        f.writelines('\n'.join(list(map(str, loss_history))))
 
-    test_file = r'/home/msreznik/PythonProjects/DICT/data/test.jpg'
-    # test_file = r'data/test.jpg'
-    generated_text = test_picture(device, model, tokenizer, image_processor, test_file)
+    # loss_history, model, tokenizer, image_processor = train(device, model, tokenizer, image_processor, init_dataloader())
+    # print(loss_history)
+    # with open('loss_history.txt', 'w', encoding='utf-8') as f:
+    #     f.writelines('\n'.join(list(map(str, loss_history))))
+
+    # test_file = r'/home/msreznik/PythonProjects/DICT/data/test.jpg'
+    test_file = r'data/test.jpg'
+    generated_text = test_picture(device, model, tokenizer, image_processor, test_file, verbose=False)
     print(f'Test image caption: {generated_text}')
